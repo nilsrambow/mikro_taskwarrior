4741b993c42d6ec04afd367df2313232a4b11eb5
nilsbroders <nils_brodersen@posteo.de>
2026-01-13 18:49:30 +0100

    changed to reading the data path from configuration

--- lua/mikro_taskwarrior/config.lua
+++ lua/mikro_taskwarrior/config.lua
@@ -1,7 +1,16 @@
 local M = {}
 
--- Path to the tasks.json file
-M.TASKS_FILE = vim.fn.stdpath "data" .. "/mikro_taskwarrior/tasks.json"
+-- Default path to the tasks.json file
+local default_tasks_file = vim.fn.stdpath "data" .. "/mikro_taskwarrior/tasks.json"
+M.TASKS_FILE = default_tasks_file
+
+-- Setup function to configure the plugin
+function M.setup(opts)
+  opts = opts or {}
+  if opts.tasks_file then
+    M.TASKS_FILE = opts.tasks_file
+  end
+end
 
 return M
 
--- lua/mikro_taskwarrior/init.lua
+++ lua/mikro_taskwarrior/init.lua
@@ -1,10 +1,14 @@
 local commands = require "mikro_taskwarrior.commands"
 local task = require "mikro_taskwarrior.core.task"
+local config = require "mikro_taskwarrior.config"
 
 local M = {}
 
 -- Setup the plugin
-function M.setup()
+function M.setup(opts)
+  -- Configure the plugin with user options
+  config.setup(opts)
+  -- Setup commands
   commands.setup()
 end
 
@@ -14,7 +18,7 @@ M.add_task = task.add_task
 M.mark_task_done_by_id = task.mark_task_done_by_id
 M.modify_tasks = task.modify_tasks
 
--- Auto-setup when plugin is loaded
+-- Auto-setup when plugin is loaded (with default options)
 M.setup()
 
 return M
